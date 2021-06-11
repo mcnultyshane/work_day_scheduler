d4e38ad41196d7fa8811f2c6f819297ca3a912f1
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-11 12:27:51 -0400

    adding comments to js

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,11 +1,14 @@
+// establish variable for time with moment()
 var timeOfDay = moment().format("HH");
 console.log(timeOfDay);
+// establishing variable for only the numerical value of the hour from the time of day
 var timeInteger = parseInt(timeOfDay);
 console.log(timeInteger);
+// establishing jquery to access the 'saveBtn' on the html via its class.
 var saveBtn = $('.saveBtn');
 
 
-
+// setting the data attributes for each hour row and we will use to compare to time of day for css styling
 $("#9Row").attr("data-time", moment("9:00am", "h:mm a").format("HH"));
 $("#10Row").attr("data-time", moment("10:00am", "h:mm a").format("HH"));
 $("#11Row").attr("data-time", moment("11:00am", "h:mm a").format("HH"));
@@ -19,9 +22,8 @@ $("#6Row").attr("data-time", moment("6:00pm", "h:mm a").format("HH"));
 
 
 $(document).ready(function() {
-
-    renderPlans();
-
+    // local storage function.  see below
+    planData();
 
     var currentDay = moment().format('dddd, MMMM Do');
     console.log(currentDay);
@@ -34,11 +36,9 @@ $(document).ready(function() {
         var inputTime = $('#' + j + "Row").attr("data-time");
         var inputTimeInt = parseInt(inputTime);
         console.log(inputTime);
-
-
+        //  if loop for setting CSS styles based on the time of day and what applies to 'past', 'present', 'future'
         if (timeInteger === inputTimeInt) {
-            $("#" + j + "Row").addClass("present");       
-        
+            $("#" + j + "Row").addClass("present");        
         }
 
         if (timeInteger > inputTimeInt) {
@@ -50,6 +50,8 @@ $(document).ready(function() {
         }
     };
 
+
+// setting up mouse event for saving the data (first setting up the new class to do hover)
     saveBtn.on("mouseenter", function () {
         $(this).addClass('hover');
     });  
@@ -58,21 +60,19 @@ $(document).ready(function() {
         $(this).removeClass('hover');
     });
 
-
+// saving the plans for each row into local storage
     saveBtn.on("click", function () {
         
         var hour = $(this).attr("data-hour");
 
-      
         var plan = $("#" + hour + "Row").val();
 
-        
         localStorage.setItem(hour, plan);
 
     });
 
-
-    function renderPlans() {
+// function for pulling the local storage and populating in the proper time slot.
+    function planData() {
         
         for (var i = 1; i <= 12; i++) {
             
